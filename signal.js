48f3b29d9e9aa7dff6da4d3087947a59e896c8c4
Kshitija Sanjiv Savairam <98964354+SavairamK@users.noreply.github.com>
2023-12-25 12:15:33 +0530

    Add files via upload

---
+++ signal.js
@@ -0,0 +1,10 @@
+function stop(){
+    document.getElementById('signal').src="./red signal.jpg"
+}
+function wait(){
+    document.getElementById('signal').src="./yellow signal.jpg"
+}
+function go(){
+    document.getElementById('signal').src="./green signal.jpg"
+    alert("Happy Journey !!!\nDrive Safely !!")
+}
